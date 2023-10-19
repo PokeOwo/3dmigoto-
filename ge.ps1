3bf9a0804aeb94250a35e4ff65f84840a5be4ad4
PokeOwo <92107859+PokeOwo@users.noreply.github.com>
2023-10-19 19:28:03 +0800

    Add files via upload

--- ge.ps1
+++ ge.ps1
@@ -7,18 +7,33 @@ $loaderPath = Join-Path $scriptDir "3DMigoto Loader.exe"
 # 啟動 3DMigoto Loader.exe 並要求管理員權限
 Start-Process -FilePath $loaderPath -Verb RunAs
 
-# 等待 3 秒
-Start-Sleep -Seconds 3
+# 等待 3DMigoto Loader.exe 輸出指定的文字
+$foundText = $false
+while (-not $foundText)
+{
+    $loaderOutput = Get-Content $loaderPath
+    if ($loaderOutput -match "3DMigoto ready - Now run the game.")
+    {
+        $foundText = $true
+    }
+    Start-Sleep -Milliseconds 100
+}
 
 # 啟動 GenshinImpact.exe 並要求管理員權限
 Start-Process -FilePath "C:\Program Files\Genshin Impact\Genshin Impact Game\GenshinImpact.exe" -Verb RunAs
 
-# 持續監視 GenshinImpact.exe 是否已經啟動，並在啟動後自動切換視窗
-$giWindow = $null
-while ($giWindow -eq $null)
+# 等待 3DMigoto Loader.exe 輸出指定的文字，以確保 GenshinImpact.exe 已經啟動
+$foundText = $false
+while (-not $foundText)
 {
+    $loaderOutput = Get-Content $loaderPath
+    if ($loaderOutput -match "GenshinImpact.exe")
+    {
+        $foundText = $true
+    }
     Start-Sleep -Milliseconds 100
-    $giWindow = Get-Process | Where-Object { $_.ProcessName -eq "GenshinImpact" } | Select-Object -First 1 | Select-Object -ExpandProperty MainWindowHandle
 }
-$null = (New-Object -ComObject Shell.Application).ToggleDesktop()  # 切換到桌面
-$null = (New-Object -ComObject WScript.Shell).AppActivate($giWindow)  # 切換到 GenshinImpact.exe 視窗
+
+# 切換到 GenshinImpact.exe 視窗
+$giWindow = Get-Process | Where-Object { $_.ProcessName -eq "GenshinImpact" } | Select-Object -First 1 | Select-Object -ExpandProperty MainWindowHandle
+$null = (New-Object -ComObject WScript.Shell).AppActivate($giWindow)
